b3701e0cf7e006f220e8e4f8ab731bf2d9d13a47
Samuel Grundman <samuel@yodas.ws>
2014-01-11 00:14:10 -0500

    OAuth2.0 Login is Working
    file_get_contents wrecks app usability on HTTP 400 error. My
    httpWorker::request can react to such problems.
    Hopefully you just need to setup your own keys.php and direct users to
    /login.php?service=fb

--- login.php
+++ login.php
@@ -1,8 +1,17 @@
 <?php
 session_start();
+if (isset($_SERVER['HTTP_REFERER'])) {
+	$_SESSION['prelogin'] = $_SERVER['HTTP_REFERER'];
+	if (strstr($_SERVER['HTTP_REFERER'], '?')) {
+		$referer = substr($_SERVER['HTTP_REFERER'], 0, strpos($_SERVER['HTTP_REFERER'], '?'));
+	} else $referer = $_SERVER['HTTP_REFERER'];
+	if ($_SERVER['SCRIPT_URL'] == $referer) {
+		$_SESSION['prelogin'] = substr($_SERVER['HTTP_REFERER'], 0, strpos($_SERVER['HTTP_REFERER'], ':')) . "://{$_SERVER['HTTP_HOST']}";
+	}
+} else $_SESSION['prelogin'] = substr($_SERVER['SCRIPT_URI'], 0, strpos($_SERVER['SCRIPT_URI'], ':')) . "://{$_SERVER['HTTP_HOST']}";
 
 // First, Ask for Permission
-if (empty($_GET['code']) and !empty($_REQUEST['service'])) {
+if (empty($_SESSION['user']['fb']) and empty($_SESSION['tokens']['fb']) and empty($_GET['code']) and !empty($_REQUEST['service'])) {
 	// Services Using OAuth2.0
 	if (in_array($_REQUEST['service'], array(
 		'fb',
@@ -10,14 +19,17 @@ if (empty($_GET['code']) and !empty($_REQUEST['service'])) {
 		require_once('oauth2.php');
 		OAuth2::getPermission($_REQUEST['service'], "/login?service={$_REQUEST['service']}");
 		// If we're here, there was an error
+		unset($_SESSION['state']);
 		$_SESSION['error'] = array('Could not login');
-		header('Location: /');
+		header('HTTP/1.1 403 Unauthorized');
+		header("Location: {$_SESSION['prelogin']}");
+		unset($_SESSION['prelogin']);
 		exit;
 	}
 }
 
 // Second, Now Login
-if ($_REQUEST['state'] == $_SESSION['state'] and !empty($_GET['code'])) {
+if (empty($_SESSION['fbtoken']) and !empty($_REQUEST['state']) and !empty($_SESSION['state']) and $_REQUEST['state'] == $_SESSION['state'] and !empty($_GET['code'])) {
 	// Services Using OAuth2.0
 	if (in_array($_REQUEST['service'], array(
 		'fb',
@@ -25,14 +37,20 @@ if ($_REQUEST['state'] == $_SESSION['state'] and !empty($_GET['code'])) {
 		require_once('oauth2.php');
 		OAuth2::login($_REQUEST['service']);
 		// If we're here, there was an error
-		header('Location: /');
+		$_SESSION['error'] = array('Could not login');
+		header('HTTP/1.1 403 Unauthorized');
+		header("Location: {$_SESSION['prelogin']}");
+		unset($_SESSION['prelogin']);
 		exit;
 	}
-	switch ($_REQUEST['service']) {
-	case 'fb':
-		require_once('facebook.php');
-		break;
-	}
 }
+switch ($_REQUEST['service']) {
+case 'fb':
+	require_once('facebook.php');
+	break;
+}
+// Successfully Logged In !
+header("Location: {$_SESSION['prelogin']}");
+unset($_SESSION['prelogin']);
+exit;
 ?>
-<h1>Login</h1>
--- oauth2.php
+++ oauth2.php
@@ -2,22 +2,24 @@
 require_once('keys.php');
 class OAuth2 {
 	protected $access_token;
-	public static $auth_url = ''; // Ask for Permissions
-	public static $token_url = ''; // Grab Access Token
-	public static $user_url = ''; // Grab User Info
+	const auth_url = ''; // Ask for Permissions
+	const token_url = ''; // Grab Access Token
 
 	public function __construct($options) {
 	}
 
 	public static function getPermission($service, $login_url) {
 		if (strpos($login_url, 'http://') !== 0 and strpos($login_url, 'https://') !== 0)
-			$login_url = substr($_SERVER['SCRIPT_URI'], 0, strpos($_SERVER['SCRIPT_URI'], '//')+2) . "{$_SERVER['HTTP_HOST']}$login_url";
+			$login_url = substr($_SERVER['SCRIPT_URI'], 0, strpos($_SERVER['SCRIPT_URI'], ':')) . "://{$_SERVER['HTTP_HOST']}$login_url";
 		$url = $login_url;
-		$_SESSION['state'] = md5(uniqid(rand(100,999)));
+		$_SESSION['state'] = md5(uniqid(rand(100,999))) . '%' . $login_url;
 		switch ($service) {
 		case 'fb':
 			require_once('facebook.php');
-			$url = Facebook::$auth_url . '?client_id=' . Keys::$fb['client_id'] . "&state={$_SESSION['state']}&redirect_uri=" . urlencode($login_url);
+			$url = Facebook::auth_url .
+				'?client_id=' . Keys::$fb['client_id'] .
+				'&state=' . urlencode($_SESSION['state']) .
+				'&redirect_uri=' . urlencode($login_url);
 			break;
 		default:
 			return false;
@@ -27,22 +29,38 @@ class OAuth2 {
 	}
 
 	public static function login($service) {
+		list($state, $login_url) = explode('%', $_SESSION['state'], 2);
+		unset($_SESSION['state']);
 		switch ($service) {
 		case 'fb':
-			$url = Facebook::$token_url . '?client_id=' . Facebook::$client_id . '&client_secret=' . Facebook::$secret .  '&code=' . $_GET['code'] .  '&redirect_uri=' . urlencode($login_url);
-			$fb_user_response = file_get_contents(Facebook::$user_url . $_SESSION['fb_token']);
+			require_once('facebook.php');
+			$url = Facebook::token_url .
+				'?client_id=' . Keys::$fb['client_id'] .
+				'&client_secret=' . Keys::$fb['secret'] . 
+				'&code=' . $_GET['code'] . 
+				'&redirect_uri=' . urlencode($login_url);
+			require_once('http.class.php');
+			$fb_user_response = '';
+			try {
+				$http = httpWorker::request($url);
+				$fb_user_response = $http['response'];
+			} catch (Exception $e) {
+				$_SESSION['error'] = array('Could not log into Facebook');
+				header('HTTP/1.1 500 Error');
+				header("Location: {$_SESSION['prelogin']}");
+				unset($_SESSION['prelogin']);
+				exit;
+			}
 			// TODO: Check HTTP Status Code
 			if ($fb_user_response) {
 				$params = null;
 				parse_str($fb_user_response, $params);
-echo '<pre>' . print_r($params, true); exit;
-				$_SESSION['fb_token'] = $params['access_token'];
+				$_SESSION['tokens']['fb'] = $params['access_token'];
 				unset($params);
 			}
+			unset($http);
 			unset($fb_user_response);
-			list($state, $redirect) = explode('%', $_SESSION['state'], 2);
-			unset($_SESSION['state']);
-			header("Location: $hostname$redirect");
+			header("Location: $login_url");
 			exit;
 		}
 	}
