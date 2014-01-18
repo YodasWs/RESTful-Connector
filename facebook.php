e846c73019173bca51242cd3b81504258cb410b8
Samuel Grundman <samuel@yodas.ws>
2014-01-18 13:10:26 -0500

    Load User Data after Login
    and save to $_SESSION

--- facebook.php
+++ facebook.php
@@ -4,9 +4,11 @@ class Facebook extends OAuth2 {
 	const auth_url = 'https://www.facebook.com/dialog/oauth';
 	const token_url = 'https://graph.facebook.com/oauth/access_token';
 
-	private $user_url = 'https://graph.facebook.com/me?access_token=';
+	const user_url = 'https://graph.facebook.com/me?access_token=';
 
 	public function __construct($options) {
+		if (!empty($_SESSION['tokens']['fb']) and empty($_SESSION['user']['fb']))
+			$this->loadUser();
 		parent::__construct($options);
 	}
 
@@ -19,7 +21,7 @@ class Facebook extends OAuth2 {
 	}
 
 	public function loadUser() {
-		$fb_user_response = file_get_contents($fb['user_url'] . $_SESSION['tokens']['fb']);
+		$fb_user_response = file_get_contents(self::user_url . $_SESSION['tokens']['fb']);
 		$_SESSION['user']['fb'] = json_decode($fb_user_response, true);
 		$_SESSION['user']['fb']['image'] = "https://graph.facebook.com/{$_SESSION['user']['fb']['id']}/picture";
 	}
@@ -40,5 +42,7 @@ class Facebook extends OAuth2 {
 
 // TODO: Load User Data
 if (!empty($_SESSION['tokens']['fb']) and empty($_SESSION['user']['fb'])) {
-	$_SESSION['user']['fb'] = array('id' => 'me');
+	if (empty($_SESSION['facebook']) or get_class($_SESSION['facebook']) != 'Facebook')
+		$_SESSION['facebook'] = new Facebook();
+#	$_SESSION['user']['fb'] = array('id' => 'me');
 }
